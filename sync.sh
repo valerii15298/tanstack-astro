4a82a2c39b7c42c1f44d790349ae77caf2a731bb
Valerii Petryniak <valerii15298@gmail.com>
2026-02-24 21:26:24 +0200

    add sync script to automate documentation updates

---
+++ sync.sh
@@ -0,0 +1,7 @@
+set -e
+
+pnpx degit --force TanStack/router/docs/router ./src/content/docs/router
+
+pnpx degit --force TanStack/router/docs/start/framework/react ./src/content/docs/start
+
+node add-title.js
